c4cd564bce54b89fdb85c247c29d6f7c062a624c
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-12 10:33:42 +1000

    Add files via upload

---
+++ random-fractions-with-Javascript.js
@@ -0,0 +1,5 @@
+const numerator = Math.floor(Math.random() * 10) + 1;
+const denominator = Math.floor(Math.random() * 10) + 1;
+
+// Display fraction in the console
+console.log(`${numerator}/${denominator}`);
