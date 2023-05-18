819d3bd1af940b703b94d3ed14bdefd955e65722
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-19 08:31:04 +1000

    Add files via upload

---
+++ for-loops.js
@@ -0,0 +1,26 @@
+ const daysTillAlbury = [];
+
+for (let i = 0; i <= 10; i++) {
+    daysTillAlbury.push(i); 
+        if (i === 10) {
+            console.log("You're going on a Holiday!")
+        } else {
+            console.log(i);
+        }
+    }
+
+
+/*
+function generateEvenNumbers(limit) {
+  const evenNumbers = [];
+
+  for (let i = 0; i <= limit; i += 2) {
+    evenNumbers.push(i);
+  }
+
+  return evenNumbers;
+}
+
+const evenNumberSequence = generateEvenNumbers(20);
+console.log(evenNumberSequence);
+*/
