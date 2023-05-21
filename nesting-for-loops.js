1f15e6903dec1356cedd3f630508cf06c18392b9
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-21 11:41:23 +1000

    Add files via upload

--- nesting-for-loops.js
+++ nesting-for-loops.js
@@ -13,3 +13,40 @@ for (let i = 1; i <= tableSize; i++) {
     // separator after each row
     console.log('-'.repeat(10));
 }
+/*
+const myStock = {
+    "category_1": {
+      "item_1": {
+        "name": "Soil",
+        "price": 10
+      },
+      "item_2": {
+        "name": "Fertilizer",
+        "price": 15
+      }
+    },
+    "category_2": {
+      "item_3": {
+        "name": "Banksia",
+        "price": 20
+      },
+      "item_4": {
+        "name": "Fern Tree",
+        "price": 30
+      }
+    },
+    "category_3": {
+      "item_5": {
+        "name": "Lilly",
+        "price": 15
+      },
+      "item_6": {
+        "name": "Roses",
+        "price": 30
+      }
+    }
+  };
+  
+  console.log(myStock.category_2.item_3.name);
+  console.log(myStock.category_3.item_6.price);
+  */
