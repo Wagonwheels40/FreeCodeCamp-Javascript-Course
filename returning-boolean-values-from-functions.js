1384938743260db54ba543c5d83b638c5ca44789
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-26 10:21:12 +1000

    Add files via upload

---
+++ returning-boolean-values-from-functions.js
@@ -0,0 +1,23 @@
+/* function scoreIs(hsv, stp) {
+    // Only change code below this line
+    if (hsv > stp) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(scoreIs(10, 15))
+*/
+
+function scoreIs(hsv, stp) {
+    if (hsv === 2 && stp === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(scoreIs(2, 1))
+
+
