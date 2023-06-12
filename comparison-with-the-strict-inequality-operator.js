13ea8a919f61c1cb555e272d9df8c7a2af8e0b0b
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-12 16:11:37 +1000

    Add files via upload

---
+++ comparison-with-the-strict-inequality-operator.js
@@ -0,0 +1,12 @@
+function costOfMilkshake(val) {
+
+    if (val !== 3) {
+
+        return "That's not how much it costs";
+    }
+    return "Here you go!";
+}
+
+// Change this value to test
+console.log(costOfMilkshake(3));
+    
