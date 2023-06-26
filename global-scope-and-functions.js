80f692e6e479d80edaf9acec2e3db6bb06064c7c
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-27 09:30:32 +1000

    Add files via upload

---
+++ global-scope-and-functions.js
@@ -0,0 +1,22 @@
+var couhtriesTraveled = 10;
+var countriesLived;
+
+function fun1() {
+    countriesLived = 3;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof countriedTraveled !== "undefined") {
+        output += "countriesTraveled: " + countriesTraveled;
+    }
+    if (typeof countriesLived !== "undefined") {
+        output += "countriesLived: " + countriesLived;
+    }
+    console.log(output);
+}
+
+console.log(countriesLived);
+
+fun1();
+fun2();
