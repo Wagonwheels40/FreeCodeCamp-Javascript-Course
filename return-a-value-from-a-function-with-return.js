4379e66d1bef6c8d672036ef1ecb90fd4e1a36cf
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-29 09:57:33 +1000

    Add files via upload

---
+++ return-a-value-from-a-function-with-return.js
@@ -0,0 +1,11 @@
+function discountedPrice(num) {
+    return num - 3;
+}
+
+function normalPrice(num) {
+    return num + 5;
+}
+
+console.log("The price is now " + discountedPrice(10));
+console.log("The price is now " + normalPrice(5));
+
