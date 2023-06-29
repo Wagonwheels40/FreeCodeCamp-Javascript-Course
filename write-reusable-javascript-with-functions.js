483afa35a25f1ee64dcee3c2b5182f3527c2e5e4
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-30 09:01:02 +1000

    Add files via upload

---
+++ write-reusable-javascript-with-functions.js
@@ -0,0 +1,11 @@
+function endOfLooneyTunes() {
+    console.log("That's all Folks!");
+}
+
+endOfLooneyTunes();
+
+function phoneOrderWentThrough() {
+    console.log("Congratulations, you have recharged!");
+}
+
+phoneOrderWentThrough();
