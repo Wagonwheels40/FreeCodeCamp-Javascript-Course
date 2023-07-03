e883af645d36ee874ee55db870f1474e5c9f7eb1
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-03 11:01:49 +1000

    Add files via upload

---
+++ manipulate-arrays-with-shift.js
@@ -0,0 +1,10 @@
+var ourArray = ["Cat Stevens", "Yusuf", ["Cat"]];
+var removedFromOurArray = ourArray.shift();
+
+var myArray = [["Mark", 32], ["Gold Fish", 1]];
+
+console.log(myArray, " <= initial");
+
+var removedFromMyArray = myArray.shift();
+
+console.log(myArray, " <= post shift");
