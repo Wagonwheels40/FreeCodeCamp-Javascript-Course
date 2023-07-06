21f5eb827a8402117733739422430dc30b20f6bd
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-06 10:05:08 +1000

    Add files via upload

---
+++ manipulate-arrays-with-pop.js
@@ -0,0 +1,17 @@
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+
+var myArray = [["John", 23], ["cat", 2]];
+
+console.log(myArray);
+
+var removedFromMyArray = myArray.pop();
+
+console.log(myArray);
+console.log(removedFromMyArray);
+
+ourArray = [4, 5, 6];
+myArray = [["Jane", 30], ["dog", 3]];
+
+console.log(ourArray);
+console.log(myArray);
