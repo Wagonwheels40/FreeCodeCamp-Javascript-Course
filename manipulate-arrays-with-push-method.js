8f85de450198f71344acd420cb50d3d6a6db0162
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-12 16:23:36 +1000

    Add files via upload

---
+++ manipulate-arrays-with-push-method.js
@@ -0,0 +1,8 @@
+var springfieldResident = ["Homer", "Simpson", "Springfield Resident"];
+springfieldResident.push(["Goofy", "Happy"]);
+
+var kidsOfSimpsons = [["Marge", 45], ["Springfield Resident"]];
+kidsOfSimpsons.push(["Bart Simpson", 12]);
+
+console.log(springfieldResident, " <= after push");
+console.log(kidsOfSimpsons, " <= after push");
