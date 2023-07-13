94e444d75237f032e740431b6c487158e9e6f880
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-13 12:39:23 +1000

    Add files via upload

---
+++ access-multi-dimensional-arrays-with-indexes.js
@@ -0,0 +1,13 @@
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2];
+
+console.log(myData);
+
+-----------------
+
+var minutesPerJog = [[10,15,20], [25,30,35], [40,45,50], [55,60,65]];
+
+var joggingTimes = minutesPerJog[1];
+
+console.log("I jog for " + joggingTimes + " minutes");
