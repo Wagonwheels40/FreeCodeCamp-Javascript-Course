7dc0b6a7831f56c0ae0a55dad17e06687e903ccf
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-17 08:58:00 +1000

    Add files via upload

---
+++ access-array-data-with-indexes.js
@@ -0,0 +1,15 @@
+var ourArray = [50,60,70];
+var ourData = ourArray[0];
+
+var myArray = [50,60,70];
+
+var myData = myArray[0];
+
+console.log(myData);
+
+// Some of my own //
+
+var cateringFor = [10,15,20,25];
+var mealsFor = cateringFor[3];
+
+console.log(mealsFor);
