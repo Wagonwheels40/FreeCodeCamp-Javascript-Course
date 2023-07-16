0effb945bb592e6ebf39dcc4f75f9275f8d256f6
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-17 09:09:04 +1000

    Add files via upload

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,13 @@
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = [["this is the first position", 11], ["the second position", 22]];
+
+console.log(myArray[0][1]);
+
+// Change it below //
+
+var geogaphyNumbers = [["Countries in Euriope", 44], ["Countries in Asia", 48]];
+
+var bridgesInBigCities = [["Hamburg", 2500], ["New York", 2000]];
+
+console.log(geogaphyNumbers[0][1]);
