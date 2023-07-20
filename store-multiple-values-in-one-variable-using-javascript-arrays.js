9e0ee70ced63a305f6bb3aee374bf7209c3eceba
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-21 09:40:57 +1000

    Add files via upload

---
+++ store-multiple-values-in-one-variable-using-javascript-arrays.js
@@ -0,0 +1,24 @@
+var ourArray = ["John", 23];
+
+var myArray = ["22", 22];
+
+var integerTypes = 22 + 2
+
+console.log(integerTypes, " <= integerTypes");
+
+var stringTypes = "22" + "2"
+
+console.log(stringTypes, " <= stringTypes");
+
+var valueTypes = typeof 22
+
+console.log(valuesTypes, " <= valueTypes");
+
+// Last ones are some of my own ones
+
+var myNameIs = ["Garreth"]; 
+
+var myAgeIs = ["31"];  
+
+console.log(myNameIs, " <= Name"); 
+console.log(myAgeIs, " <= Age"); 
