1feaf1a7bff856e44e7113aa667bc81da40a7ce6
Daniel <116485787+Wagonwheels40@users.noreply.github.com>
2023-08-29 16:50:36 +1000

    Add files via upload

---
+++ word-blanks.js
@@ -0,0 +1,14 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+
+/* var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+console.log(wordBlanks); */
+// Example is above - Below is mine //
+var phoneIAmAbleToBuy = "Samsung Galaxy S9.";
+var phoneIWantToBuy = "Purism Librem 5, ";
+
+var wordBlanks2 = "Today I went to the store to buy a " + phoneIWantToBuy + "but instead, I got a " + phoneIAmAbleToBuy;
+console.log(wordBlanks2);
