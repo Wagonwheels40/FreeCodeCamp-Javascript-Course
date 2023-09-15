f2505fc0199705f620b1c9bdb0e371071fd47813
Daniel <116485787+Wagonwheels40@users.noreply.github.com>
2023-09-15 16:16:06 +1000

    Add files via upload

---
+++ use-bracket-notation-to-find-the-Nth-to-last-character-in-a-string.js
@@ -0,0 +1,13 @@
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+
+// Above is FreeCodeCamp solution - Below is my own //
+
+var nativeAustralianAnimals = "Koala";
+var fifthToLastLetterOfNAA = nativeAustralianAnimals[nativeAustralianAnimals.length - 5];
+console.log(fifthToLastLetterOfNAA);
