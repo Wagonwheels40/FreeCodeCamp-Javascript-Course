844a07a6f02f31915957be6bdaef33f2f1b35689
Daniel <116485787+dkwagner40@users.noreply.github.com>
2023-09-19 20:18:26 +1000

    Add files via upload

---
+++ find-the-length-of-a-string.js
@@ -0,0 +1,18 @@
+// Below is the solution for freeCodeCamp //
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+// My own code // 
+
+var petNameLength = 0;
+var petName = "Winston";
+
+petNameLength = petName.length;
+console.log(petName.length)
