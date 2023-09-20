31090eb4a1f7d6e28c539742d643caae5e14c92c
Daniel <116485787+dkwagner40@users.noreply.github.com>
2023-09-20 17:54:49 +1000

    Add files via upload

---
+++ appending-variables-to-strings.js
@@ -0,0 +1,13 @@
+var someAdjective = "my intention";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+// Below is my own code //
+
+var commonJapaneseVerb = "ikimasu";
+var japaneseSentence = "Ashita wa Su-Pa ni ";
+
+japaneseSentence += commonJapaneseVerb;
+
+// console.log(japaneseSentence); //
